6784b0613e343e12ae19bca233525cce8e6c53a1
Lenny P Critchley <doaltplusf4@gmail.com>
2020-05-07 17:50:27 +0100

    Initial commit.
    Co-authored-by: Shroomba241 <harwoodw241@gmail.com>

---
+++ main.py
@@ -0,0 +1,64 @@
+import pyglet
+from pyglet.window import key, mouse
+
+
+class Application:
+    def __init__(self):
+        self.loadResources()
+        self.window = pyglet.window.Window()
+        self.window.push_handlers(self)
+
+        self.key_handler = key.KeyStateHandler()
+        self.window.push_handlers(self.key_handler)
+
+        self.sprite = pyglet.sprite.Sprite(
+            self.resources["blue"],
+            x=self.window.width//2,
+            y=self.window.height//2
+        )
+
+    def loadResources(self):
+        self.resources = {}
+
+        self.resources["blue"] = pyglet.resource.image(
+            "resources/square_blue.png"
+        )
+        self.resources["red"] = pyglet.resource.image(
+            "resources/square_red.png"
+        )
+
+    def on_draw(self):
+        self.window.clear()
+        self.sprite.draw()
+
+    def update(self, dt):
+        movement_speed = 60
+
+        if self.key_handler[key.LSHIFT]:
+            movement_speed *= 20
+
+        if (
+            (self.key_handler[key.A] or
+             self.key_handler[key.D]) and
+            (self.key_handler[key.W] or
+             self.key_handler[key.S])
+        ):
+            movement_speed /= 2 ** 0.5
+
+        if self.key_handler[key.W]:
+            self.sprite.y += movement_speed * dt
+        if self.key_handler[key.A]:
+            self.sprite.x -= movement_speed * dt
+        if self.key_handler[key.S]:
+            self.sprite.y -= movement_speed * dt
+        if self.key_handler[key.D]:
+            self.sprite.x += movement_speed * dt
+
+    def run(self):
+        pyglet.clock.schedule_interval(self.update, 1/60)
+        pyglet.app.run()
+
+
+if __name__ == "__main__":
+    application = Application()
+    application.run()
