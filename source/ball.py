f3a905d4813e44b754c012e333d9e4e401cc91dc
Lenny P Critchley <doaltplusf4@gmail.com>
2020-05-08 16:51:27 +0100

    Adding collision detection.

--- source/ball.py
+++ source/ball.py
@@ -14,3 +14,18 @@ class Ball(Basic):
         )
         self.vx = random.choice([-200, 200])
         self.vy = random.randint(-150, 150)
+
+    def update(self, dt):
+        if (
+            self.aabb[0] < 0 or
+            self.aabb[2] > self.application.window.width
+        ):
+            self.vx *= -1
+
+        if (
+            self.aabb[1] < 0 or
+            self.aabb[3] > self.application.window.height
+        ):
+            self.vy *= -1
+
+        super().update(dt)
