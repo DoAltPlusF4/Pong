16fb31d4baeab2d29bee1ee0c391d03b6d4483e0
Lenny P Critchley <doaltplusf4@gmail.com>
2020-05-08 18:16:38 +0100

    Added visual borders.

--- main.py
+++ main.py
@@ -16,6 +16,19 @@ class Application:
         self.key_handler = key.KeyStateHandler()
         self.window.push_handlers(self.key_handler)
 
+        self.bottom_border = pyglet.shapes.Rectangle(
+            0, 0,
+            self.window.width, 30,
+            color=(255, 255, 255),
+            batch=self.batch
+        )
+        self.top_border = pyglet.shapes.Rectangle(
+            0, self.window.height,
+            self.window.width, -30,
+            color=(255, 255, 255),
+            batch=self.batch
+        )
+
         self.ball = Ball(self)
 
     def on_draw(self):
