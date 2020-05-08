9c509d4867a125f340b7f166fce2946a5cade54b
Lenny P Critchley <doaltplusf4@gmail.com>
2020-05-08 20:31:30 +0100

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,18 @@
+import sys
+from cx_Freeze import setup, Executable
+
+# Dependencies are automatically detected, but it might need
+# fine tuning.
+buildOptions = dict(packages=["source", "pyglet"], excludes=[])
+
+base = 'Win32GUI' if sys.platform == 'win32' else None
+
+executables = [
+    Executable('main.py', base=base, targetName='Pong')
+]
+
+setup(name='Pong',
+      version='1.0',
+      description="It's pong.",
+      options=dict(build_exe=buildOptions),
+      executables=executables)
